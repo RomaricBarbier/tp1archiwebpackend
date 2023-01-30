d6ff05fdf5afebb96a2f39e43d1d27571a60095e
Romaric Barbier <romaric.barbier@etud.univ-jfc.fr>
2023-01-30 10:09:57 +0100

    commit 2

--- fie-3-cours-spring-jpa-fb928ddbf888/src/main/resources/data.sql
+++ fie-3-cours-spring-jpa-fb928ddbf888/src/main/resources/data.sql
@@ -6,5 +6,11 @@ INSERT INTO Country(id, code, name) VALUES
 -- On peut fixer la valeur des clés auto-générées, mais il faut ensuite
 -- réinitialiser le compteur de clé auto-générée
 -- Attention : la syntaxe est différente selon le SGBD utilisé
+
+INSERT INTO CITY(name, population, country_id) VALUES
+                                                   ('Paris', 12, 1),
+                                                   ('London', 18, 2),
+                                                   ('New York', 27, 3);
+
 ALTER TABLE Country ALTER COLUMN id RESTART WITH 4;
 
